2b77e475560572aefcc6d821b96fb8c461152026
pratchayapol <pratchayapol2543@gmail.com>
2023-12-26 10:45:16 +0700

    main

---
+++ src/dormbookings/classes/DBConnection.php
@@ -0,0 +1,31 @@
+<?php
+if(!defined('mysql')){
+    require_once(__DIR__ . '/../initialize.php');
+}
+class DBConnection{
+
+    private $host = 'mysql';
+    private $username = DB_USERNAME;
+    private $password = DB_PASSWORD;
+    private $database = DB_NAME;
+    
+    public $conn;
+    
+    public function __construct(){
+
+        if (!isset($this->conn)) {
+            
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+            
+            if (!$this->conn) {
+                echo 'Cannot connect to database server';
+                exit;
+            }            
+        }    
+        
+    }
+    public function __destruct(){
+        $this->conn->close();
+    }
+}
+?>
